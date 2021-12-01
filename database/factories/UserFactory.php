11bf2c1c79533d4ce0d3ac63f1efefb1f8bff0a9
asare847 <gideonasare847@outlook.com>
2021-12-01 17:19:27 +0000

    procfile added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
+use Faker\Factory as Faker;
 class UserFactory extends Factory
 {
     /**
